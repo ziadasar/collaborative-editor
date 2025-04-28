25bd61a7fa47a7dea7828794128e88ea36d2f8d6
ziadasar <ziadasar@gmail.com>
2025-04-28 12:43:54 +0300

    spring boot project

---
+++ src/main/java/com/apt/collaborative_editor/CollaborativeEditorApplication.java
@@ -0,0 +1,13 @@
+package com.apt.collaborative_editor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollaborativeEditorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollaborativeEditorApplication.class, args);
+	}
+
+}
